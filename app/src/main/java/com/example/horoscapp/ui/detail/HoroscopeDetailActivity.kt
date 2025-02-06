b3ad959256163635b1de7216406e1b01979e2557
Tania Rivera <tania.riverah@elektra.com.mx>
2025-02-05 22:25:06 -0600

    added image detail horoscope

--- app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -8,7 +8,20 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
+import com.example.horoscapp.R
 import com.example.horoscapp.databinding.ActivityHoroscopeDetailBinding
+import com.example.horoscapp.domain.model.HoroscopeModel.Aquarius
+import com.example.horoscapp.domain.model.HoroscopeModel.Aries
+import com.example.horoscapp.domain.model.HoroscopeModel.Cancer
+import com.example.horoscapp.domain.model.HoroscopeModel.Capricorn
+import com.example.horoscapp.domain.model.HoroscopeModel.Gemini
+import com.example.horoscapp.domain.model.HoroscopeModel.Leo
+import com.example.horoscapp.domain.model.HoroscopeModel.Libra
+import com.example.horoscapp.domain.model.HoroscopeModel.Pisces
+import com.example.horoscapp.domain.model.HoroscopeModel.Sagittarius
+import com.example.horoscapp.domain.model.HoroscopeModel.Scorpio
+import com.example.horoscapp.domain.model.HoroscopeModel.Taurus
+import com.example.horoscapp.domain.model.HoroscopeModel.Virgo
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -25,7 +38,7 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUI()
-        horoscopeDetailViewModel.getHoroscope(args.type.name)
+        horoscopeDetailViewModel.getHoroscope(args.type)
     }
 
     private fun initUI() {
@@ -34,7 +47,7 @@ class HoroscopeDetailActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        //Boton atras
+        //Button back
         binding.ivBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
 
@@ -57,6 +70,22 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         binding.pb.isVisible = false
         binding.tvTitle.text = state.sign
         binding.tvBody.text = state.prediction
+
+        val image = when (state.horoscopeModel) {
+            Aries -> R.drawable.detail_aries
+            Taurus -> R.drawable.detail_taurus
+            Gemini -> R.drawable.detail_gemini
+            Cancer -> R.drawable.detail_cancer
+            Leo -> R.drawable.detail_leo
+            Virgo -> R.drawable.detail_virgo
+            Libra -> R.drawable.detail_libra
+            Scorpio -> R.drawable.detail_scorpio
+            Sagittarius -> R.drawable.detail_sagittarius
+            Capricorn -> R.drawable.detail_capricorn
+            Aquarius -> R.drawable.detail_aquarius
+            Pisces -> R.drawable.detail_pisces
+        }
+        binding.ivDetail.setImageResource(image)
     }
 
     private fun loadingState() {
--- app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -2,6 +2,7 @@ package com.example.horoscapp.ui.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.horoscapp.domain.model.HoroscopeModel
 import com.example.horoscapp.domain.usecase.GetPredictionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -18,15 +19,19 @@ class HoroscopeDetailViewModel @Inject constructor(private val getPredictionUseC
     private var _state = MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading)
     val state: StateFlow<HoroscopeDetailState> = _state
 
-    fun getHoroscope(sign: String) {
+    lateinit var horoscope: HoroscopeModel
+
+    fun getHoroscope(sign: HoroscopeModel) {
+        horoscope = sign
         viewModelScope.launch {
             //Hilo principal
             _state.value = HoroscopeDetailState.Loading
             val result =
-                withContext(Dispatchers.IO) { getPredictionUseCase(sign) } //Hilo secundario
+                withContext(Dispatchers.IO) { getPredictionUseCase(sign.name) } //Hilo secundario
             //Hilo principal
             if (result != null) {
-                _state.value = HoroscopeDetailState.Success(result.horoscope, result.sign)
+                _state.value =
+                    HoroscopeDetailState.Success(result.horoscope, result.sign, horoscope)
             } else {
                 _state.value =
                     HoroscopeDetailState.Error("Ha ocurrido un error, intentelo mas tarde")
